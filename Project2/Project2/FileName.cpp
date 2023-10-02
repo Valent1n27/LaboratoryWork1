b2954a0cf41971d19d35228827b73d003300e107
Valent1n27 <144728703+Valent1n27@users.noreply.github.com>
2023-10-02 08:47:46 +0300

    Add files via upload

---
+++ Project2/Project2/FileName.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Ukrainian");
+	cout << " Hello!!!" << endl << endl << endl << endl;
+	cout <<  " ---------------------------"<< endl;
+	cout << "|                           |" << endl;
+	cout << "|                           |" << endl;
+	cout << "|                           |" << endl;
+	cout << "|                           |" << endl;
+	cout << "|                           |" << endl;
+	cout << "|                           |" << endl;
+	cout << "|                           |" << endl;
+	cout << "|                           |" << endl;
+	cout << "|                           |" << endl;
+	cout << "|                           |" << endl;
+	cout << "|                           |" << endl;
+	cout << " ---------------------------" << endl << endl << endl;
+	cout << "      Surname : Pomoz" << endl;
+	cout << "          Name: Валентин" << endl;
+	cout << "   Patronymic : Ivanovich" << endl;
+	cout << "        Gender: Male" << endl;
+	cout << " Date of Birth: 27.01.2006" << endl;
+	cout << "       Address: Lane Mariupolsky 38" << endl;
+	cout << "  Phone number: +380965907045" << endl;;
+	cout << "Place of study: FPM.DNU" << endl;
+	cout << "         Group: PA-23-1" << endl;
+	cout << "         Hobby: Programing" << endl;
+	cin.get();
+
+}
